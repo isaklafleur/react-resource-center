c979db6c1160391d86f8ae5d0e48e5acba6ec59c
Isak La Fleur <isak.engdahl@gmail.com>
2017-08-11 17:49:16 +0200

    changed to the stable version of material-UI.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import AppBar from 'material-ui/AppBar';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
+    injectTapEventPlugin();
     return (
       <div className="App">
+        <AppBar title="Title" iconClassNameRight="muidocs-icon-navigation-expand-more" />
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
