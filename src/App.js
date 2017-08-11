f95e6116912c70aefc7e2f3c8381f81e884b105f
Isak La Fleur <isak.engdahl@gmail.com>
2017-08-11 20:19:15 +0200

    added router links

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 import AppBar from 'material-ui/AppBar';
 import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
 import injectTapEventPlugin from 'react-tap-event-plugin';
-import './App.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { red500 } from 'material-ui/styles/colors';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+const muiTheme = getMuiTheme({
+  palette: {
+    primary1Color: red500,
+  },
+  appBar: {
+    height: 70,
+  },
+});
 injectTapEventPlugin();
 
 class App extends Component {
@@ -16,22 +27,57 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <AppBar
-          title="Title"
-          iconClassNameRight="muidocs-icon-navigation-expand-more"
-          onLeftIconButtonTouchTap={this.handleToggle}
-        />
-        <Drawer
-          docked={false}
-          width={200}
-          open={this.state.open}
-          onRequestChange={open => this.setState({ open })}
-        >
-          <MenuItem onTouchTap={this.handleClose}>Menu Item</MenuItem>
-          <MenuItem onTouchTap={this.handleClose}>Menu Item 2</MenuItem>
-        </Drawer>
-      </div>
+      <BrowserRouter>
+        <MuiThemeProvider muiTheme={muiTheme}>
+          <div>
+            <AppBar
+              title="Resource Center"
+              iconClassNameRight="muidocs-icon-navigation-expand-more"
+              onLeftIconButtonTouchTap={this.handleToggle}
+            />
+            <Drawer
+              docked={false}
+              width={200}
+              open={this.state.open}
+              onRequestChange={open => this.setState({ open })}
+            >
+              <MenuItem onTouchTap={this.handleClose}>
+                <Link to="/">Home</Link>
+              </MenuItem>
+              <MenuItem onTouchTap={this.handleClose}>
+                <Link to="/logos">Logos</Link>
+              </MenuItem>
+              <MenuItem onTouchTap={this.handleClose}>
+                <Link to="/posters">Posters</Link>
+              </MenuItem>
+              <MenuItem onTouchTap={this.handleClose}>
+                <Link to="/letterhead">Letterhead</Link>
+              </MenuItem>
+              <MenuItem onTouchTap={this.handleClose}>
+                <Link to="/share-a-story">Share a story</Link>
+              </MenuItem>
+              <MenuItem onTouchTap={this.handleClose}>
+                <Link to="/service-request-form">Service Request Form</Link>
+              </MenuItem>
+              <MenuItem onTouchTap={this.handleClose}>
+                <Link to="/tutorial">Tutorial</Link>
+              </MenuItem>
+            </Drawer>
+
+            <Route exact path="/" render={() => <h1>Home</h1>} />
+            <Route exact path="/logos" render={() => <h1>Logos</h1>} />
+            <Route exact path="/posters" render={() => <h1>Posters</h1>} />
+            <Route exact path="/letterhead" render={() => <h1>Letterhead</h1>} />
+            <Route exact path="/share-a-story" render={() => <h1>Share a story</h1>} />
+            <Route
+              exact
+              path="/service-request-form"
+              render={() => <h1>Service Request Form</h1>}
+            />
+            <Route exact path="/tutorial" render={() => <h1>Tutorial</h1>} />
+          </div>
+        </MuiThemeProvider>
+      </BrowserRouter>
     );
   }
 }
